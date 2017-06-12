2479cd3ecfe48f20d23b0ef5c4c248bbb7f9ef31
Martin Halamíček <martin@halamicek.cz>
2017-06-12 13:41:21 +0200

    deploy to ECR

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+docker pull quay.io/keboola/developer-portal-cli-v2:latest
+export REPOSITORY=`docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.ex-db-mssql`
+docker tag keboola/ex-db-mssql:latest $REPOSITORY:$TRAVIS_TAG
+docker tag keboola/ex-db-mssql:latest $REPOSITORY:latest
+eval $(docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.ex-db-mssql)
+docker push $REPOSITORY:$TRAVIS_TAG
+docker push $REPOSITORY:latest
