8f6258e4f1f5b2c45dc59eeb85ae69f2fcd85a24
pivnicek <marc@keboola.com>
2018-10-18 16:20:44 +0200

    tests for incremental fetching

--- tests/Keboola/DbExtractor/AbstractMSSQLTest.php
+++ tests/Keboola/DbExtractor/AbstractMSSQLTest.php
@@ -80,7 +80,7 @@ abstract class AbstractMSSQLTest extends ExtractorTest
             \"_Weir%d I-D\" INT IDENTITY(1,1) NOT NULL, 
             \"Weir%d Na-me\" VARCHAR(55) NOT NULL DEFAULT 'mario',
             \"type\" VARCHAR(55) NULL,
-            \"timestamp\" DATETIME NULL DEFAULT '2018-08-14 10:43:18'
+            \"timestamp\" DATETIME NOT NULL DEFAULT GETDATE()
             )"
         );
         $this->pdo->exec("ALTER TABLE [auto Increment Timestamp] ADD CONSTRAINT PK_AUTOINC PRIMARY KEY (\"_Weir%d I-D\")");
---
+++ tests/Keboola/DbExtractor/IncrementalFetchingTest.php
@@ -0,0 +1,153 @@
+<?php
+
+
+namespace Keboola\DbExtractor\Tests;
+
+use Keboola\DbExtractor\Exception\UserException;
+
+class IncrementalFetchingTest extends AbstractMSSQLTest
+{
+    public function testIncrementalFetchingByTimestamp(): void
+    {
+        $config = $this->getIncrementalFetchingConfig();
+        $config['parameters']['incrementalFetchingColumn'] = 'timestamp';
+        $result = ($this->createApplication($config))->run();
+        $this->assertEquals('success', $result['status']);
+        $this->assertEquals(
+            [
+                'outputTable' => 'in.c-main.auto-increment-timestamp',
+                'rows' => 2,
+            ],
+            $result['imported']
+        );
+        //check that output state contains expected information
+        $this->assertArrayHasKey('state', $result);
+        $this->assertArrayHasKey('lastFetchedRow', $result['state']);
+        $this->assertNotEmpty($result['state']['lastFetchedRow']);
+        sleep(2);
+        // the next fetch should be empty
+        $emptyResult = ($this->createApplication($config, $result['state']))->run();
+        $this->assertEquals(0, $emptyResult['imported']['rows']);
+        sleep(2);
+        //now add a couple rows and run it again.
+        $this->pdo->exec('INSERT INTO auto_increment_timestamp ([Weir%d Na-me]) VALUES (\'charles\'), (\'william\')');
+        $newResult = ($this->createApplication($config, $result['state']))->run();
+        //check that output state contains expected information
+        $this->assertArrayHasKey('state', $newResult);
+        $this->assertArrayHasKey('lastFetchedRow', $newResult['state']);
+        $this->assertGreaterThan(
+            $result['state']['lastFetchedRow'],
+            $newResult['state']['lastFetchedRow']
+        );
+    }
+    public function testIncrementalFetchingByAutoIncrement(): void
+    {
+        $config = $this->getIncrementalFetchingConfig();
+        $config['parameters']['incrementalFetchingColumn'] = '_Weir%d I-D';
+        $result = ($this->createApplication($config))->run();
+        $this->assertEquals('success', $result['status']);
+        $this->assertEquals(
+            [
+                'outputTable' => 'in.c-main.auto-increment-timestamp',
+                'rows' => 2,
+            ],
+            $result['imported']
+        );
+        //check that output state contains expected information
+        $this->assertArrayHasKey('state', $result);
+        $this->assertArrayHasKey('lastFetchedRow', $result['state']);
+        $this->assertEquals(2, $result['state']['lastFetchedRow']);
+        sleep(2);
+        // the next fetch should be empty
+        $emptyResult = ($this->createApplication($config, $result['state']))->run();
+        $this->assertEquals(0, $emptyResult['imported']['rows']);
+        sleep(2);
+        //now add a couple rows and run it again.
+        $this->pdo->exec('INSERT INTO auto_increment_timestamp ([Weir%d Na-me]) VALUES (\'charles\'), (\'william\')');
+        $newResult = ($this->createApplication($config, $result['state']))->run();
+        //check that output state contains expected information
+        $this->assertArrayHasKey('state', $newResult);
+        $this->assertArrayHasKey('lastFetchedRow', $newResult['state']);
+        $this->assertEquals(4, $newResult['state']['lastFetchedRow']);
+        $this->assertEquals(2, $newResult['imported']['rows']);
+    }
+    public function testIncrementalFetchingLimit(): void
+    {
+        $config = $this->getIncrementalFetchingConfig();
+        $config['parameters']['incrementalFetchingLimit'] = 1;
+        $result = ($this->createApplication($config))->run();
+        $this->assertEquals('success', $result['status']);
+        $this->assertEquals(
+            [
+                'outputTable' => 'in.c-main.auto-increment-timestamp',
+                'rows' => 1,
+            ],
+            $result['imported']
+        );
+        //check that output state contains expected information
+        $this->assertArrayHasKey('state', $result);
+        $this->assertArrayHasKey('lastFetchedRow', $result['state']);
+        $this->assertEquals(1, $result['state']['lastFetchedRow']);
+        sleep(2);
+        // the next fetch should contain the second row
+        $result = ($this->createApplication($config, $result['state']))->run();
+        $this->assertEquals(
+            [
+                'outputTable' => 'in.c-main.auto-increment-timestamp',
+                'rows' => 1,
+            ],
+            $result['imported']
+        );
+        //check that output state contains expected information
+        $this->assertArrayHasKey('state', $result);
+        $this->assertArrayHasKey('lastFetchedRow', $result['state']);
+        $this->assertEquals(2, $result['state']['lastFetchedRow']);
+    }
+    public function testIncrementalFetchingInvalidColumns(): void
+    {
+        $config = $this->getIncrementalFetchingConfig();
+        $config['parameters']['incrementalFetchingColumn'] = 'fakeCol'; // column does not exist
+        try {
+            $result = ($this->createApplication($config))->run();
+            $this->fail('specified autoIncrement column does not exist, should fail.');
+        } catch (UserException $e) {
+            $this->assertStringStartsWith("Column [fakeCol]", $e->getMessage());
+        }
+        // column exists but is not auto-increment nor updating timestamp so should fail
+        $config['parameters']['incrementalFetchingColumn'] = 'weird-Name';
+        try {
+            $result = ($this->createApplication($config))->run();
+            $this->fail('specified column is not auto increment nor timestamp, should fail.');
+        } catch (UserException $e) {
+            $this->assertStringStartsWith("Column [weird-Name] specified for incremental fetching", $e->getMessage());
+        }
+    }
+    public function testIncrementalFetchingInvalidConfig(): void
+    {
+        $config = $this->getIncrementalFetchingConfig();
+        $config['parameters']['query'] = 'SELECT * FROM auto_increment_timestamp';
+        unset($config['parameters']['table']);
+        try {
+            $result = ($this->createApplication($config))->run();
+            $this->fail('cannot use incremental fetching with advanced query, should fail.');
+        } catch (UserException $e) {
+            $this->assertStringStartsWith("Invalid Configuration", $e->getMessage());
+        }
+    }
+
+    protected function getIncrementalFetchingConfig(): array
+    {
+        $config = $this->getConfigRow(self::DRIVER);
+        unset($config['parameters']['query']);
+        $config['parameters']['table'] = [
+            'tableName' => 'auto_increment_timestamp',
+            'schema' => 'test',
+        ];
+        $config['parameters']['incremental'] = true;
+        $config['parameters']['name'] = 'auto-increment-timestamp';
+        $config['parameters']['outputTable'] = 'in.c-main.auto-increment-timestamp';
+        $config['parameters']['primaryKey'] = ['_Weir%d I-D'];
+        $config['parameters']['incrementalFetchingColumn'] = '_Weir%d I-D';
+        return $config;
+    }
+}
