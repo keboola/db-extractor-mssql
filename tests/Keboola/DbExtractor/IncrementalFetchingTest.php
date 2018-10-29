91266341fdd21457676ec7c62aeafae9c1a113bf
pivnicek <marc@keboola.com>
2018-10-29 16:27:53 +0100

    remove unused lib

--- tests/Keboola/DbExtractor/IncrementalFetchingTest.php
+++ tests/Keboola/DbExtractor/IncrementalFetchingTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Keboola\DbExtractor\Tests;
 
-use Keboola\Csv\CsvFile;
 use Keboola\DbExtractor\Exception\UserException;
 
 class IncrementalFetchingTest extends AbstractMSSQLTest
