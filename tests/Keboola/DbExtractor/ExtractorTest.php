5b07da820b11d8af0d5ef876361592f845c4a937
pivnicek <marc@keboola.com>
2018-10-30 11:15:42 +0100

    update query test to use new datetime conversion

--- tests/Keboola/DbExtractor/ExtractorTest.php
+++ tests/Keboola/DbExtractor/ExtractorTest.php
@@ -160,7 +160,7 @@ class ExtractorTest extends AbstractMSSQLTest
                     "lastFetchedRow" => '2018-10-26 10:52:32',
                 ],
                 "SELECT TOP 1000 * FROM [dbo].[auto Increment Timestamp] " .
-                "WHERE CONVERT(DATETIME2, [timestamp]) > CONVERT(DATETIME2, '2018-10-26 10:52:32') ORDER BY [timestamp]",
+                "WHERE CONVERT(DATETIME2, [timestamp], 121) > CONVERT(DATETIME2, '2018-10-26 10:52:32', 121) ORDER BY [timestamp]",
             ],
         ];
     }
