006b382ef147321e12a9f42046dddda0b637e4a2
pivnicek <marc@keboola.com>
2018-12-04 11:01:38 +0100

    allow nolock for deprecated configurations

---
+++ src/Keboola/DbExtractor/Configuration/MssqlConfigurationDefinition.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\DbExtractor\Configuration;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class MssqlConfigurationDefinition extends ConfigDefinition
+{
+    public function getConfigTreeBuilder() : TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('parameters');
+        // @formatter:off
+        $rootNode
+            ->children()
+                ->scalarNode('data_dir')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end()
+                ->scalarNode('extractor_class')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end()
+                ->arrayNode('db')
+                    ->children()
+                        ->scalarNode('driver')->end()
+                        ->scalarNode('host')->end()
+                        ->scalarNode('port')->end()
+                        ->scalarNode('database')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                        ->end()
+                        ->scalarNode('user')
+                            ->isRequired()
+                        ->end()
+                        ->scalarNode('password')->end()
+                        ->scalarNode('#password')->end()
+                        ->append($this->addSshNode())
+                    ->end()
+                ->end()
+                ->arrayNode('tables')
+                    ->prototype('array')
+                        ->children()
+                            ->integerNode('id')
+                                ->isRequired()
+                                ->min(0)
+                            ->end()
+                            ->scalarNode('name')
+                                ->isRequired()
+                                ->cannotBeEmpty()
+                            ->end()
+                            ->scalarNode('query')->end()
+                            ->arrayNode('table')
+                                ->children()
+                                    ->scalarNode('schema')->end()
+                                    ->scalarNode('tableName')->end()
+                                ->end()
+                            ->end()
+                            ->arrayNode('columns')
+                                ->prototype('scalar')->end()
+                            ->end()
+                            ->scalarNode('outputTable')
+                                ->isRequired()
+                                ->cannotBeEmpty()
+                            ->end()
+                            ->booleanNode('incremental')
+                                ->defaultValue(false)
+                            ->end()
+                            ->booleanNode('enabled')
+                                ->defaultValue(true)
+                            ->end()
+                            ->arrayNode('primaryKey')
+                                ->prototype('scalar')->end()
+                            ->end()
+                            ->integerNode('retries')
+                                ->min(0)
+                            ->end()
+                            ->booleanNode('nolock')->defaultValue(false)->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+        // @formatter:on
+        return $treeBuilder;
+    }
+}
