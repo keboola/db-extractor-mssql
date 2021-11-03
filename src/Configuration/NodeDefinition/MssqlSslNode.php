8aab75057284e3482ef2845d9bf4acad2e818f4f
Michal Jurečko <michal.jurecko@gmail.com>
2021-11-03 12:48:49 +0100

    Fix MssqlSslNode

--- src/Configuration/NodeDefinition/MssqlSslNode.php
+++ src/Configuration/NodeDefinition/MssqlSslNode.php
@@ -9,13 +9,7 @@ use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 
 class MssqlSslNode extends SslNode
 {
-
-    protected function addKeyNode(NodeBuilder $nodeBuilder): void
-    {
-        // not supported
-    }
-
-    protected function addCertNode(NodeBuilder $nodeBuilder): void
+    protected function addCertAndKeyNode(NodeBuilder $nodeBuilder): void
     {
         // not supported
     }
